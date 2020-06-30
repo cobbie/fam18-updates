78d1c17246adbadbff8d6977a946539ae03cb139
Cobbie Quintos <cobbiequintos@gmail.com>
2020-06-30 22:41:18 +0800

    init commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,46 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <div>Progress: 
+      <b-progress :value="value" variant="success" animated show-progress></b-progress> </div>
+      <div>Update # :</div>
+    <div>Priority level:
+    <b-icon icon="star-fill" variant="warning"></b-icon>
+    <b-icon icon="star-fill" variant="warning"></b-icon>
+    <b-icon icon="star-fill" variant="warning"></b-icon>
+    <b-icon icon="star-fill" variant="warning"></b-icon>
+    <b-icon icon="star-fill" variant="warning"></b-icon></div>
+
+  
+  <div>Press any key to continue.</div>
+
   </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Vue from 'vue'
+import { BootstrapVue, IconsPlugin, BootstrapVueIcons, Star } from 'bootstrap-vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
+Vue.use(BootstrapVueIcons)
+Vue.component('Star', Star)
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+
+  },
+  data(){
+    return {
+      value: 25
+    }
   }
 }
 </script>
